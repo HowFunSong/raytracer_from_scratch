f8367ec64db6414c41a2c05d317c22bce9ffdbef
eric <eric811125@gmail.com>
2024-12-22 00:44:05 +0800

    metal

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "camera.hpp"
 #include "hittable.hpp"
 #include "hittable_list.hpp"
+#include "material.hpp"
 #include "sphere.hpp"
 
 #undef main
@@ -32,9 +33,16 @@ const int WIDTH = 800, HEIGHT = 600;
 int main( int argc, char *argv[] )
 {
     hittable_list world;
-
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+    
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    
+    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     camera cam;
 
@@ -42,7 +50,7 @@ int main( int argc, char *argv[] )
     cam.image_width  = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth         = 50;
-    
+
     cam.render(world);
     return 0;
 }
